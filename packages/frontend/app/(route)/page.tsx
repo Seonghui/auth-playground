fe2d758e8f0214de92fb4b1b4769ccead15826bf
Stella Yu <stellashyou@gmail.com>
2024-05-20 17:49:52 +0900

    styled components 적용

---
+++ packages/frontend/app/(route)/page.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import Image from 'next/image';
+import styles from './page.module.css';
+
+export default function Home() {
+  return (
+    <main>
+      <div
+        css={`
+          background: papayawhip;
+        `}
+      >
+        Test
+      </div>
+    </main>
+  );
+}
---
+++ packages/frontend/next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  compiler: {
+    styledComponents: true,
+  },
+  experimental: {
+    forceSwcTransforms: true,
+  },
+};
